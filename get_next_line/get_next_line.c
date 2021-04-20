bdd3e2085f983df2bfc32ab0c67f4c8560fb6d14
artacone <artacone@gmail.com>
2021-04-20 15:39:27 +0300

    refactor get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,62 +6,49 @@
  * 		* Norm
  * 		* Bonus (multiple fds)
  */
-static char	*check_rem(char **rem, char **line) // Should it free line in advance?
+static char	*trim_line(char **buf, char **line)
 {
-	char	*p_endl;
+	char	*ptr_endl;
 	char	*to_free;
 
-	p_endl = NULL;
-	*line = ft_strdup(""); // FIXME (escape free() error)
-	if (*rem != NULL)
+	ptr_endl = NULL;
+	if (*buf != NULL)
 	{
 		to_free = *line;
-		if ((p_endl = ft_strchr(*rem, '\n')) != NULL)
+		if ((ptr_endl = ft_strchr(*buf, '\n')) != NULL)
 		{
-			*p_endl = '\0';
-			*line = ft_strdup(*rem);
-			free(*rem);
-			*rem = ft_strdup(++p_endl); // FIXME replace with smth like strcpy()
+			*ptr_endl = '\0';
+			*line = ft_strjoin(*line, *buf);
+			free(*buf);
+			*buf = ft_strdup(++ptr_endl); // FIXME replace with smth like strcpy()
 		}
 		else
 		{
-			*line = ft_strdup(*rem);
-			free(*rem);
-			*rem = NULL;
+			*line = ft_strjoin(*line, *buf);
+			free(*buf);
+			*buf = NULL;
 		}
 		free(to_free);
 	}
-	return (p_endl);
+	return (ptr_endl);
 }
 
 int	get_next_line(int fd, char **line)
 {
-	char		buf[BUFFER_SIZE + 1];
+	char		buf_read[BUFFER_SIZE + 1];
 	ssize_t		n_read;
 	char 		*ptr_endl;
-	char 		*to_free;
-	static char	*rem;
+	static char	*buf;
 
-	if (BUFFER_SIZE < 1 || line == NULL || read(fd, buf, 0) == -1)
+	if (BUFFER_SIZE < 1 || line == NULL || read(fd, buf_read, 0) == -1)
 		return (-1);
-	ptr_endl = check_rem(&rem, line);
-	while ((ptr_endl == NULL) && (n_read = read(fd, buf, BUFFER_SIZE))) // FIXME protect read
+	*line = ft_strdup("");
+	ptr_endl = trim_line(&buf, line);
+	while ((ptr_endl == NULL) && (n_read = read(fd, buf_read, BUFFER_SIZE))) // FIXME protect read
 	{
-		buf[n_read] = '\0';
-		if ((ptr_endl = ft_strchr(buf, '\n')))
-		{
-			*ptr_endl = '\0';
-			rem = ft_strdup(++ptr_endl); // Should it really allocate each time?
-		}
-		to_free = *line;
-		if ((*line = ft_strjoin(*line, buf)) == NULL)
-		{
-			free(to_free);
-			if (ptr_endl)
-				free(rem);
-			return (-1); // Leak possibility
-		}
-		free(to_free); // FIXME free(rem)
+		buf_read[n_read] = '\0';
+		buf = ft_strdup(buf_read);
+		ptr_endl = trim_line(&buf, line);
 	}
 	if (ptr_endl)
 		return (1);
